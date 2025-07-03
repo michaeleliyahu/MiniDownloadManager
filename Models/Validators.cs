0174ae9a5cf2c2c7d1dbdbf23903614e0005548a
Michael Eliyahu <mmyy605@gmail.com>
2025-07-03 12:43:38 +0300

    finish MVP part

---
+++ Models/Validators.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniDownloadManager.Models
+{
+    public class Validators
+    {
+        public int? ram { get; set; }
+        public int? os { get; set; }
+        public int? disk { get; set; }
+    }
+}
